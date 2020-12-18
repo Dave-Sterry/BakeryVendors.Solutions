5cb72ac9bad75503d9d6ba7159e43e7cfd8e29d7
David Sterry <sterry.david@gmail.com>
2020-12-18 11:32:16 -0800

    add HttpGet routes to Order Controller

--- BakeryVendors/Controllers/OrdersController.cs
+++ BakeryVendors/Controllers/OrdersController.cs
@@ -2,4 +2,29 @@ using Microsoft.AspNetCore.Mvc;
 using BakeryVendors.Models;
 using System.Collections.Generic;
 
-namespace BakeryVendor.Controllers;
+namespace BakeryVendor.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+
+    public ActionResult New(in vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/(orderId}")]
+
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+  }
+}
