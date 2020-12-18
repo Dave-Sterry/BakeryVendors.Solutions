d4c2e99b422816c7839377409d5a4facc2672997
David Sterry <sterry.david@gmail.com>
2020-12-18 11:25:22 -0800

    finish routes for Vendor Controller

--- BakeryVendors/Controllers/VendorsController.cs
+++ BakeryVendors/Controllers/VendorsController.cs
@@ -29,5 +29,35 @@ namespace BakeryVendor.Controllers
       Vendor newVendor = new Vendor(vendorName);
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/vendors/{id}")]
+
+    public ActionResult Show(int id)
+    {
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    Vendor choosenVendor = Vendor.Find(id);
+    List<Order> choosenOrder = choosenVendor.Orders;
+    model.Add("vendor", choosenVendor);
+    model.Add("orders", choosenOrders);
+    return view(model);
+    }
+
+    [HttpPost("/vendors/{vendorsId/orders")]
+
+    public ActionResult Create(int vendorId, string orderTitle, string orderDescription, string orderPrice, string orderDate )
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor choosenVendor = Vendor.Find(vendorId);
+      Order choosenOrder = new Order(orderTitle, orderDescription, orderPrice, orderDescription);
+      choosenVendor.AddOrder(choosenOrder);
+      List<Order> vendorOrders = choosenOrder.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor" choosenVendor);
+      return View("Show", model);
+    }
+
+
+
+
   }
 }
