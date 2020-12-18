84a1fd166509b3fb21620535ac166c34ddee06f7
David Sterry <sterry.david@gmail.com>
2020-12-18 10:04:46 -0800

    purposefully fail Vendor List test

--- BakeryVendors.Tests/ModelTests/VendorTests.cs
+++ BakeryVendors.Tests/ModelTests/VendorTests.cs
@@ -26,5 +26,30 @@ namespace BakeryVendors.Tests
 
       Assert.AreEqual(name, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_ObjectList()
+    {
+      string name1 = "Coffeeshop";
+      string name2= "newseasons";
+      Vendor newVendor1 = new Vendor(name1);
+      Vendor newVendor2 = new Vendor(name2);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2};
+
+      List<Vendor> result = Vendor.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    // [TestMethod]
+    // public void GetId_ReturnsVendorWithId_Int()
+    // {
+    //   string name = "VendorTest";
+    //   Vendor newVendor = new Vendor(name);
+
+    //   string result = newVendor.Id;
+
+    //   Assert.AreEqual(2, result);
+    // }
   }
 }
--- BakeryVendors/Models/Vendor.cs
+++ BakeryVendors/Models/Vendor.cs
@@ -5,11 +5,21 @@ namespace BakeryVendors.Models
   
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> { };
     public string Name { get; set; }
+    public int Id { get; }
 
     public Vendor(string vendorName)
     {
       Name = vendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
+
+    public static List<Vendor> GetAll()
+    {
+      return null;
+    }
+
   }
 }
