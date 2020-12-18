5f6d98f7ef7bb8dcbb617afafeb14984570594ad
David Sterry <sterry.david@gmail.com>
2020-12-18 11:12:14 -0800

    start routes for Vendor Controller

--- BakeryVendors/Controllers/VendorsController.cs
+++ BakeryVendors/Controllers/VendorsController.cs
@@ -7,6 +7,27 @@ namespace BakeryVendor.Controllers
 {
   public class VendorsController : Controller
   {
-    [HttpGet()]
+    [HttpGet("/vendors")]
+
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
   }
 }
